bc30cb29578185e5c2a4eb8f7b65d3211729ecfe
weibinke <283142081@qq.com>
2024-02-05 17:45:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 python3 -m venv .venv
-source .venv/bin/activate
+. .venv/bin/activate
 pip3 install -r requirements.txt
 ps aux | grep search.py | awk '{print $2}' | xargs kill -9
 nohup python searchapi/search.py > log.txt &
